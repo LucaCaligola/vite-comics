7390a68fbb858c991a2e26d1d8e6f43f5c8b7f0b
LucaCaligola <luca.caligola@gmail.com>
2023-12-14 17:10:19 +0100

    added li on the header

--- vite-comics/src/components/AppHeader.vue
+++ vite-comics/src/components/AppHeader.vue
@@ -2,10 +2,21 @@
     <header>
         <div class="container">
             <div class="logo">
-                <img src="" alt="">
+                aaa 
+                <img src="../assets/img/dc-logo" alt="">
             </div>
 
             <ul>
+                <li>Chharacters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
 
             </ul>
 
@@ -31,10 +42,36 @@ header {
   
 }
 .container {
+    height: 100%;
+    width: 70%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+}
+
+.container {
     display: flex;
 }
 
+ul {
+    display: flex;
+    list-style: none;
+    
+
+}
 
 
+li {
+      margin-left: 1rem;
+      text-transform: uppercase;
+      font-size: 12px;
+      
+      &:hover {
+        color: #0282F9;
+        
+        border-bottom: 2px solid #0282f9d3;
+      }
+    }
     
 </style>
